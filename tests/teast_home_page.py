dc737193fb10cb851e9158512a05a600bde898a5
NevmerzhytskaOlya <newmerzhicka17@gmail.com>
2024-10-14 00:02:17 +0200

    added home page to test suite

--- tests/teast_home_page.py
+++ tests/teast_home_page.py
@@ -2,3 +2,8 @@ def test_home_page_dashboard():
     # Open home page
     # Check some value on the dashboard
     pass
+
+def test_home_page_menu():
+    # Open home page
+    # Check some value on the dashboard
+    pass
