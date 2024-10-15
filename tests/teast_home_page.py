ecabcd5b768172b3cf8f1e2e29f8c9dfe04a7f2b
NevmerzhytskaOlya <newmerzhicka17@gmail.com>
2024-10-15 07:37:35 +0200

    added test steps

--- tests/teast_home_page.py
+++ tests/teast_home_page.py
@@ -1,6 +1,8 @@
 def test_home_page_dashboard():
     # Open home page
+    print ("Open home page")
     # Check some value on the dashboard
+    print ("Check value")
     pass
 
 def test_home_page_menu():
